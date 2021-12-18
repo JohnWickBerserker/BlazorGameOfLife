640dbcfca26b6514897b1268156c746e1e6e26b2
johnwickberserker <johnwickberserker@gmail.com>
2021-12-18 20:46:31 +0400

    added UsingCanvas.razor

---
+++ Core/GameTemplate.cs
@@ -0,0 +1,39 @@
+﻿namespace BlazorGameOfLife.Core
+{
+    public class GameTemplate : GameOfLife
+    {
+        public int StepMs = 100;
+
+        public bool IsPlaying = true;
+        public string PlayStopButtonText = "Pause";
+
+        public GameTemplate(int rows, int cols) : base(rows, cols)
+        {
+            PlaceObject(0, 0);
+        }
+
+        public event Action LoopEvent;
+
+        public async void PlayStopAsync()
+        {
+            IsPlaying = !IsPlaying;
+            PlayStopButtonText = IsPlaying ? "Pause" : "Play";
+            await RunAsync();
+        }
+
+        public void Stop()
+        {
+            IsPlaying = false;
+        }
+
+        public async Task RunAsync()
+        {
+            while (IsPlaying)
+            {
+                Loop();
+                LoopEvent.Invoke();
+                await Task.Delay(StepMs);
+            }
+        }
+    }
+}
